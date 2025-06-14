8a20ec8e9c9e4cb9f9f2504cc7a87e21dceec103
adityadomle <adityadomle14@gmail.com>
2025-06-15 04:46:38 +0530

    Commit

--- 05-JavaScript/01-Que-1/script.js
+++ 05-JavaScript/01-Que-1/script.js
@@ -4,3 +4,9 @@ let age = prompt("Enter your age:");
 age = Number(age);
 
 // now lets check conditions 
+
+if (age < 18) {
+    console.log("You are kiddo!!!");
+} else if (age >= 18 && age <= 60) {
+    console.log("You are Adult!");
+} else (console.log("Your are Senior citizen."));
