d3c92f3387615e312fdf2f7ef1c4ab06c13f4c03
adityadomle <adityadomle14@gmail.com>
2025-06-15 05:48:54 +0530

    commit

---
+++ 05-JavaScript/04-Que-4/script.js
@@ -0,0 +1,13 @@
+let num1 = Number(prompt("Enter first Number"));
+let num2 = Number(prompt("Enter second Number"));
+let num3 = Number(prompt("Enter third Number"));
+// we take the uesr input
+
+if (num1 > num2 && num1 > num3) {
+    console.log("Largest is:" + num1);
+} else if (num2 > num3) {
+    console.log("Largest is:" + num2);
+} else {
+    console.log("Largest is:" + num3);
+
+}
