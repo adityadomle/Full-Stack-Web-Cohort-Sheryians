bf321e1b5cc3d9ff4e45c688426f94578078c2f6
Aditya Domle <adityadomle14@gmail.com>
2025-07-02 20:37:09 +0530

    This is a OOPs

---
+++ 06-JavaScript/03-OOPs-Level-1/class.js
@@ -0,0 +1,11 @@
+class Tofee {
+    constructor(flavour, color, taste, shape) {
+        this.flavour = flavour;
+        this.color = color;
+        this.taste = taste;
+        this.shape = shape;
+    }
+}
+
+let p1 = new Tofee("chocolate", "brown", "sweet","round");
+let p2 = new Tofee("vanilla", "white", "sweet","square");
