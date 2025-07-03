5373b93e5141d29c5663d688f68ac91095a28105
Aditya Domle <adityadomle14@gmail.com>
2025-07-03 10:19:45 +0530

    Level 2 OOPs

---
+++ 06-JavaScript/04-OOPs-Level-2/script.js
@@ -0,0 +1,16 @@
+function Employee(name, age, id, salary) {
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+    this.id = id;
+}
+
+Employee.prototype.printDetails = function () {
+    console.log(
+        `${this.name} is my name I am ${this.age} years old and my id is ${this.id} and my salary is ${this.salary}`
+    );
+
+};
+
+let e1 = new Employee("Aditya", 20, 1234, 75000);
+e1.printDetails();
